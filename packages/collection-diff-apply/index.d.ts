499ddcf9c20d9a24564c153cfd3ccdf8c120fcf2
kuthia <ax.nowayout@gmail.com>
2021-12-28 15:40:11 +0900

    types: collection-diff-apply

---
+++ packages/collection-diff-apply/index.d.ts
@@ -0,0 +1,10 @@
+// Definitions by: kuthia <https://github.com/kuthia>
+type Operation = "add" | "replace" | "remove";
+
+export function diffApply<T>(
+  oldValue: T,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  diffs: { op: Operation; path: any[]; value: any }[],
+): T
+
+export function jsonPatchPathConverter(path: string): string[]
